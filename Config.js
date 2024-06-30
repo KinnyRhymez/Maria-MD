7d7e422fdf58000426b0832747eb53505039199d
KinnyRhymez <nanadwira52@gmail.com>
2024-06-30 23:58:02 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "233506087430"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘Kinny_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
